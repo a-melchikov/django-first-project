45c5d0f47a3472fef4e96b1a839ef55513866da5
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-03-17 23:10:17 +0400

    Error and exception handling for the send_telegram function.

--- landingpagewebsite_root/telebot/sendmessage.py
+++ landingpagewebsite_root/telebot/sendmessage.py
@@ -3,6 +3,8 @@ from .models import TeleSettings
 
 
 def send_telegram(tg_name, tg_phone):
+    if not TeleSettings.objects.get(pk=1):
+        return
     settings = TeleSettings.objects.get(pk=1)
     token = str(settings.tg_token)
     chat_id = str(settings.tg_chat)
@@ -11,16 +13,30 @@ def send_telegram(tg_name, tg_phone):
     api = "https://api.telegram.org/bot"
     method = api + token + "/sendMessage"
 
-    part_1 = text[0 : text.find("{")]
-    part_2 = text[text.find("}") + 1 : text.rfind("{")]
-    part_3 = text[text.rfind("}") : -1]
+    if text.find("{") and text.find("}") and text.rfind("{") and text.rfind("}"):
+        part_1 = text[0 : text.find("{")]
+        part_2 = text[text.find("}") + 1 : text.rfind("{")]
+        part_3 = text[text.rfind("}") : -1]
 
-    text_slice = part_1 + tg_name + part_2 + tg_phone + part_3
+        text_slice = part_1 + tg_name + part_2 + tg_phone + part_3
+    else:
+        text_slice = text
 
-    req = requests.post(
-        method,
-        data={
-            "chat_id": chat_id,
-            "text": text_slice,
-        },
-    )
+    try:
+        req = requests.post(
+            method,
+            data={
+                "chat_id": chat_id,
+                "text": text_slice,
+            },
+        )
+    except Exception:
+        pass
+
+    finally:
+        if req.status_code != 200:
+            print("Ошибка отправки!")
+        elif req.status_code == 500:
+            print("Ошибка 500")
+        else:
+            print("Все работает сообщение отправлено!")
